8670fbd0fedbecc471d87747461e60a4e10db68a
Megan Kwok <megankwok168@gmail.com>
2025-05-19 00:09:22 -0400

    Research Project - DevSurveyData processing

---
+++ Resesarch Project/StackOverflowDevSurveyData/DevSurveyDataProcess.py
@@ -0,0 +1,28 @@
+import csv
+
+writeData = 'StackOverflowDevSurveyData\DevSurveyDataProcessed.csv'
+with open(writeData, mode='w', newline='') as f:
+    writer = csv.writer(f)
+
+#2011-2016 formatted by commas -- regular csv format
+# #2016-2020,2022,2023 formatted by semi-colons
+
+for year in list(range(2011, 2021)) + [2022, 2023]:
+    d = dict()
+    with open(f'StackOverflowDevSurveyData\DevSurvey{year}.csv', mode = 'r', newline = '') as file:
+        if year <= 2015:
+            reader = csv.reader(file)
+        else:
+            reader = csv.reader(file, delimiter=';')
+        for row in reader:
+            for value in row:
+                if value in d.keys():
+                    d[value] += 1
+                else:
+                    d[value] = 1
+    with open(writeData, mode='a', newline='') as dataFile:
+        writer = csv.writer(dataFile)
+        for i in d:
+            if i != '' and d[i] > 5:
+                writer.writerow([year,i,d[i]])    
+    print(f'{year} processed')
